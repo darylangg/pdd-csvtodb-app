1d7b98a87643d6761de5d01c979adae72f19a0a3
Daryl <daryl_ang@jtc.gov.sg>
2023-07-11 15:51:38 +0800

    code clean up

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,9 @@ FROM adoptopenjdk/openjdk11:alpine-jre
 
 RUN apk update && apk add bash
 # copy the packaged jar file into our docker image
-COPY target/pdd-csvtodb-app*.jar ./pdd-csvtodb-app.jar
+COPY target/csvtodb-app*.jar ./csvtodb-app.jar
 COPY target/application.properties ./application.properties
 
 ENV JAVA_OPTS="-XX:PermSize=1024m -XX:MaxPermSize=512m"
 # set the startup command to execute the jar
-CMD ["java","-jar","pdd-csvtodb-app.jar"]
+CMD ["java","-jar","csvtodb-app.jar"]
--- src/main/java/org/csvtodb/route/CSVRoute.java
+++ src/main/java/org/csvtodb/route/CSVRoute.java
@@ -2,7 +2,6 @@ package org.csvtodb.route;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.component.rabbitmq.RabbitMQConstants;
 import org.apache.camel.dataformat.csv.CsvDataFormat;
 import org.springframework.stereotype.Component;
 import org.apache.camel.component.jackson.JacksonDataFormat;
@@ -25,6 +24,9 @@ public class CSVRoute extends RouteBuilder {
     @Value("${csv.completedDirectory}")
     private String completedDirectory;
 
+    @Value("${csv.errorDirectory}")
+    private String errorDirectory;
+
     @Value("${csv.cronInterval}")
     private String cronInterval;
 
@@ -44,6 +46,9 @@ public class CSVRoute extends RouteBuilder {
         String fileOutURI = "file:"
                 .concat(completedDirectory);
 
+        String fileErrorURI = "file:"
+                .concat(errorDirectory);
+
         from(fileReadURI)
             .routeId("CSV Scheduler Route")
                 .log("Processing ${headers.CamelFileName}")
@@ -51,43 +56,59 @@ public class CSVRoute extends RouteBuilder {
                 exchange.setProperty("data", exchange.getIn().getBody());
                 exchange.setProperty("csvHeaders", exchange.getIn().getHeaders());
             })
-            .to("direct:sendToDB")
+            .to("direct:sendToDB");
+
+        from("direct:sendToDB")
+            .routeId("DB Route")
+            .doTry()
+                .unmarshal(csvDataFormat)
+                .process(exchange -> {
+                    // Get the CSV data
+                    List<List<String>> csvData = (List<List<String>>) exchange.getIn().getBody();
+
+                    String fileName = (String) exchange.getMessage().getHeader(Exchange.FILE_NAME);
+                    Path filePath = Paths.get(fileName);
+                    String vertical = filePath.getParent().toString();
+
+                    // Convert each row to a JSON object
+                    List<Map<String, Object>> jsonObjects = new ArrayList<>();
+                    for (int j=1; j < csvData.size(); j++){
+                        List<String> row = csvData.get(j);
+                        Map<String, Object> jsonObject = new LinkedHashMap<>();
+                        for (int i = 0; i < row.size(); i++) {
+                            // Use the first row as keys for the JSON object
+                            String key = csvData.get(0).get(i);
+                            String value = row.get(i);
+                            jsonObject.put(key, value);
+                        }
+                        jsonObjects.add(jsonObject);
+                    }
+                    // Set the JSON objects as the new body
+                    exchange.getIn().setBody(jsonObjects);
+                    exchange.getIn().removeHeaders("*");
+                    exchange.getIn().setHeader("vertical", vertical);
+                })
+                .marshal(jsonDataFormat)
+                .toD("http://"+ hapiUrl +"/api/v1/${headers.vertical}/data")
+                .removeHeaders("*")
+                .setProperty("status", constant("success"))
+                .to("direct:fileMove")
+            .doCatch(Exception.class)
+                .log("Error occurred: ${exception.message}")
+                .setProperty("status", constant("error"))
+                .to("direct:fileMove");
+
+        from("direct:fileMove")
+            .routeId("File Move Route")
             .process(exchange->{
                 exchange.getIn().setBody(exchange.getProperty("data"));
                 exchange.getIn().setHeaders((Map<String, Object>) exchange.getProperty("csvHeaders"));
             })
-            .to(fileOutURI);
-
-        from("direct:sendToDB")
-                .routeId("DB Route")
-            .unmarshal(csvDataFormat)
-            .process(exchange -> {
-                // Get the CSV data
-                List<List<String>> csvData = (List<List<String>>) exchange.getIn().getBody();
-
-                String fileName = (String) exchange.getMessage().getHeader(Exchange.FILE_NAME);
-                Path filePath = Paths.get(fileName);
-                String vertical = filePath.getParent().toString();
-
-                // Convert each row to a JSON object
-                List<Map<String, Object>> jsonObjects = new ArrayList<>();
-                for (int j=1; j < csvData.size(); j++){
-                    List<String> row = csvData.get(j);
-                    Map<String, Object> jsonObject = new LinkedHashMap<>();
-                    for (int i = 0; i < row.size(); i++) {
-                        // Use the first row as keys for the JSON object
-                        String key = csvData.get(0).get(i);
-                        String value = row.get(i);
-                        jsonObject.put(key, value);
-                    }
-                    jsonObjects.add(jsonObject);
-                }
-                // Set the JSON objects as the new body
-                exchange.getIn().setBody(jsonObjects);
-                exchange.getIn().removeHeaders("*");
-                exchange.getIn().setHeader("vertical", vertical);
-            })
-            .marshal(jsonDataFormat)
-            .toD("http://"+ hapiUrl +"/api/v1/${headers.vertical}/data");
+            .log("${headers.CamelFileName} : ${exchangeProperty.status}")
+            .choice()
+            .when(simple("${exchangeProperty.status} == 'success'"))
+                .to(fileOutURI)
+            .when(simple("${exchangeProperty.status} == 'error'"))
+                .to(fileErrorURI);
     }
 }
